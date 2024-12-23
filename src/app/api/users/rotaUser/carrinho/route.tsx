937574b81d82c5c7ecf3239749749eec18dbc94b
leandro camargo more <112519932+leandrocmore@users.noreply.github.com>
2024-12-23 20:28:27 -0300

    testedee

--- src/app/api/users/rotaUser/carrinho/route.tsx
+++ src/app/api/users/rotaUser/carrinho/route.tsx
@@ -2,14 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import PrismaGlobal from "@/lib/PrismaGlobal/PrismaGlobal";
 
 // Em seu arquivo principal (por exemplo, server.js ou index.js)
-import "../../../../../lib/controleApi/limpezaCarrinhosCron";
-//import { limparCarrinhosExpirados } from "@/lib/controleApi/LimpezaAtualizador";
+// crow desativado import "../../../../../lib/controleApi/limpezaCarrinhosCron";
+import { limparCarrinhosExpirados } from "@/lib/controleApi/LimpezaAtualizador";
 // Garantindo que o cron job seja iniciado
 
 //import { limparCarrinhosExpirados} from "../../../../../lib/controleApi/" 
 
 // Iniciar limpeza automática de carrinhos expirados
-//setInterval(limparCarrinhosExpirados , 1000)
+setInterval(limparCarrinhosExpirados , 1000)
 
 //Validação do número de telefone (opcional)
 //const telefoneValido = /^\d{2}\d{9}$/; // Exemplo: 15996982622
